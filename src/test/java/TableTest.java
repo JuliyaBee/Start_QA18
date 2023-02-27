f46db2b5ccc8d5f6d4ac584cfa4f0b5a4368c458
JuliyaBee <juliyabee@gmail.com>
2023-02-27 17:43:41 +0200

    HW_6

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -1,5 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+
 public class TableTest {
 
+WebDriver wd;
+@BeforeMethod
+    public  void preCondition() {
+    System.out.println("Opening web-site");
+    wd = new ChromeDriver();
+    wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
+    }
+    //public void tableHW2(){
+        // print amount of table rows
+        // print amount of table columns
+        // find locator for the row3
+        // find locator for the last column
+    @Test
+            public void tableHW2(){
+        List<WebElement>columns=wd.findElements(By.cssSelector("table#customers.ws-table-all th"));
+        System.out.println("Amount of table columns are " +columns.size());
+        List<WebElement>rows=wd.findElements(By.cssSelector("table#customers.ws-table-all td"));
+        System.out.println("Amount of table rows are "+ rows.size()/columns.size());
+        List<WebElement>row3=wd.findElements(By.cssSelector("table#customers.ws-table-all tbody:nth-child(4)"));
+         List<WebElement>lastColmn=wd.findElements(By.cssSelector("table#customers.ws-table-all tbody:nth-child(3n+3"));
+    }
 
-
+    @AfterMethod
+    public void postCondition(){
+    wd.quit();
+    }
 }
+
+
